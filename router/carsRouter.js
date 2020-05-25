159f7152987222f1e18f73299784e77dc46a7bcc
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-05-24 23:18:34 -0400

    updated the router file to add delete and patch

--- router/carsRouter.js
+++ router/carsRouter.js
@@ -14,7 +14,7 @@ router.get("/", (req, res) => {
         })
 })
 
-router.get("/", (req, res) => {
+router.get("/:id", (req, res) => {
     const { id } = req.params
     db('cars')
         .where({ id })
@@ -32,11 +32,45 @@ router.post('/', (req, res) => {
     db('cars')
         .insert(carData)
         .then((car) => {
-            res.status(201).json(car)
+            res.status(201).json({ message: "Created Successfully" })
             console.log("Created a new car")
         })
         .catch((err) => {
             res.status(500).json({ message: "Failed too add car", err })
+        })
+})
+
+router.patch("/:id", (req, res) => {
+    const changes = req.body
+    const { id } = req.params
+    db('cars')
+        .where({ id })
+        .update * changes
+            .then(num => {
+                if (num > 0) {
+                    res.status(200).json({ message: 'Updated Successfully' })
+                } else {
+                    res.status(404).json({ message: 'Error with updating' })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'error' })
+            })
+})
+
+router.delete("/:id", (req, res) => {
+    db('cars')
+        .where('id', req.params.id)
+        .del()
+        .then(deleted => {
+            if (deleted) {
+                res.status(200).json({ message: 'Deleted Successfully' })
+            } else {
+                res.status(404).json({ message: 'Error Deleting' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'error' })
         })
 })
 
